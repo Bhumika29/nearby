1b14af44f0eda64df0919063ffab0872d38be25f
Bhumika29 <46344760+Bhumika29@users.noreply.github.com>
2019-03-12 00:21:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 //const uuidv1 = require('uuid/v1');
 const request=require("request");
+var deasync = require('deasync');
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -26,24 +27,45 @@ app.post('/webhook',(req,res) =>{
       ? req.body.result.parameters.places
       : "restaurants";
 	  
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types;
-    console.log(url);
- var req=request(url,function (err,response,body){
-
-      var places = JSON.parse(body);
-	  var w="";
-	 for(i=0;i<5;i++)
-	 {
-		 w=w+"  "+places.results[i].name;
-	 }
-      console.log(w);
-    });
+	var w=getPlaces(type);
+	
 	 return res.json({
     speech: w,
     displayText: w,
     source: "webhook-echo-sample"
   });
-}); 
+});
+var result;
+function getPlaces(type)
+{
+	
+  var key = "AIzaSyCJuRDLJZNS5yO2MhWxlCN-4FnC4L1Rs8g";
+  var location = "-33.8670522,151.1957362";
+  var radius = 16000;
+//  var types="restaurants";
+  var sensor = false;
+	result=undefined;
+  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types;
+    console.log(url);
+ var req=request(url,function (err,response,body){
+
+      var places = JSON.parse(body);
+	  var result="";
+	/* for(i=0;i<5;i++)
+	 {
+		 w=w+"  "+places.results[i].name;
+	 }*/
+	 result=places.results[0].name;
+      console.log(result);
+    });
+	
+	while(result == undefined){
+		require('deasync').runLoopOnce();
+	}
+	return result;
+}
+	
+ 
 app.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
   
