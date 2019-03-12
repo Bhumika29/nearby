cb3c402a59750b422d9226a6ce6d1c5cf4c7307f
Bhumika29 <46344760+Bhumika29@users.noreply.github.com>
2019-03-12 09:22:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+
+
+
 'use strict';
+
 const express = require("express");
 const bodyParser = require("body-parser");
 //const uuidv1 = require('uuid/v1');
 const request=require("request");
+var google = require('google');
 var deasync = require('deasync');
 const app = express();
 app.use(bodyParser.json());
@@ -11,65 +16,59 @@ app.use(
     extended: true
   })
 );
-
-
-  var key = "AIzaSyCJuRDLJZNS5yO2MhWxlCN-4FnC4L1Rs8g";
-  var location = "-33.8670522,151.1957362";
-  var radius = 16000;
- var types="restaurants";
-  var sensor = false;
-  
+const path=require("path");
+//const server=require("http").createServer(app);
+//const io=require("socket.io")(server);
 app.post('/webhook',(req,res) =>{
-  
-  var types =
+var types =
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.places
       ? req.body.result.parameters.places
       : "restaurants";
-	  
+	 
 	var w=getPlaces(types);
-	console.log(w);
 	
-	 return res.json({
+	return res.json({
     speech: w,
     displayText: w,
     source: "webhook-echo-sample"
   });
+  
+	
+	
 });
 var result;
 function getPlaces(types)
 {
-	
-  var key = "AIzaSyCJuRDLJZNS5yO2MhWxlCN-4FnC4L1Rs8g";
+	var key = "AIzaSyCJuRDLJZNS5yO2MhWxlCN-4FnC4L1Rs8g";
   var location = "-33.8670522,151.1957362";
   var radius = 16000;
 //  var types="restaurants";
   var sensor = false;
+  
 	result=undefined;
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types;
-    console.log(url);
- var req=request(url,function (err,response,body){
-
-      var places = JSON.parse(body);
 	
-	/* for(i=0;i<5;i++)
-	 {
-		 w=w+"  "+places.results[i].name;
-	 }*/
-	 result=places.results[0].name;
-    //  console.log(result);
-    });
+	var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types;
+	console.log(url);
+	var req=request(url,function (err,response,body){
+	if(err)
+		console.log(err);
+	var places=JSON.parse(body);
+	console.log(places);
 	
+	
+		result=places.results[0].name;
+	
+});
 	while(result == undefined){
 		require('deasync').runLoopOnce();
 	}
+		
 	return result;
 }
-	
- 
+
+
 app.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
-  
- 
 });
